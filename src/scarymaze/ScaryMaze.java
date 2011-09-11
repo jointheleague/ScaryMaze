5ebc75896ebccbfe205f1cfc3bde4d98fbc76459
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-09-11 14:58:57 -0700

    Create a blank window

---
+++ src/scarymaze/ScaryMaze.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scarymaze;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author avh4
+ */
+public class ScaryMaze extends JComponent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Maze Game");
+        window.add(new ScaryMaze());
+        window.pack();
+        window.setLocationRelativeTo(null); // Center on the screen
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(1000, 800);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        
+        g.setColor(Color.PINK.brighter());
+        
+    }
+    
+    
+    
+    
+}
